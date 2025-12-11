0c0f0772a430f20dc0869b3b7781c5bc36e1ca3c
TheCobraBoy2 <landoncs10@gmail.com>
2025-12-10 19:15:53 -0600

    Remove old vsix from build directory

--- scripts/create-build.js
+++ scripts/create-build.js
@@ -7,9 +7,21 @@ const __dirname = path.dirname(__filename);
 
 const buildPath = path.join(__dirname, "..", "build");
 
+// Ensure build folder exists
 if (!fs.existsSync(buildPath)) {
     fs.mkdirSync(buildPath, { recursive: true });
     console.log("Build folder created:", buildPath);
 } else {
     console.log("Build folder already exists:", buildPath);
 }
+
+// Delete old .vsix files
+const files = fs.readdirSync(buildPath);
+
+files.forEach(file => {
+    if (file.endsWith(".vsix")) {
+        const filePath = path.join(buildPath, file);
+        fs.unlinkSync(filePath);
+        console.log("Deleted old VSIX file:", filePath);
+    }
+});
